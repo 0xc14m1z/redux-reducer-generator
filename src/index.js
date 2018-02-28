328ac0597aa93ee8368daac8e9468ba81e8dd391
Luca Ramundo <luca@ramundo.info>
2018-02-28 07:38:54 +0100

    Draft implementation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
-const reducer = (initialState, map) => {}
+const reducer = (initialState, map = {}) =>
+  validate(map) &&
+  (state = initialState, action = {}) =>
+    map[action.type]
+      ? map[action.type](state, action, initialState)
+      : state
 
 export default reducer
 module.exports = reducer
+
+const validate = map => {
+  if ( isObject(map) ) {
+    if ( Object.values(map).every(value => isFunction(value)) ) {
+      return true
+    } else {
+      throw new TypeError("The map must contain only methods.")
+    }
+  } else {
+    throw new TypeError("The map must be an object.")
+  }
+}
+
+const isObject = obj =>
+  !!obj &&
+  Object.getPrototypeOf(obj) === Object.prototype
+
+const isFunction = f =>
+  typeof func === "function"
